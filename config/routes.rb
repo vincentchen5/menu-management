be9598457e5cfa967c0e28db80d444412fe82fa3
Vincent <vin2013.vc@gmail.com>
2022-04-08 22:17:03 -0400

    User create verification

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-# Defines the root path route ("/")
-# root "articles#index"
 
 Rails.application.routes.draw do
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
   root to: "main#index"
 
